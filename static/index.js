c80eb5b6a3a917d4bff7fd6cc619968c2066cf38
Devdutt Shenoi <devdutt@outlook.in>
2021-02-04 20:49:54 +0530

    Initial commit

--- static/index.js
+++ static/index.js
@@ -1,8 +1,35 @@
+function getEntries() {
+    return axios({
+        method: method,
+        url: url,
+        data: data,
+        xsrfCookieName: 'csrftoken',
+        xsrfHeaderName: 'X-CSRFToken',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    })
+}
+
 var app = new Vue({
     el: '#app',
     data: {
-      entries: [
-          {code: 'abc', name: 'ABC', open: 123, high: 456, low: 0, close: 200}
-      ]
+        key: 'Search',
+        entries: [
+            {code: 'abc', name: 'ABC', open: 123, high: 456, low: 0, close: 200}
+        ]
+    },
+    created() {
+        let hd = this;
+        getEntries('', 'get').then((res) => {
+            hd.entries = response.data.entries
+        });
+    },
+    methods: {
+        filterEntries() {
+            let hd = this;
+            let formData = new FormData();
+            formData.append('title', this.search)
+        }
     }
-  })
+})
