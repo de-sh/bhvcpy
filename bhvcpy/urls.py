b4749e59624ce7adb9f93a0e3dc55b7059b74ee5
Devdutt Shenoi <devdutt@outlook.in>
2021-02-05 13:51:54 +0530

    Style fixtures

--- bhvcpy/urls.py
+++ bhvcpy/urls.py
@@ -16,4 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("bhvcpy/", include("srv.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("srv.urls"))]
--- srv/admin.py
+++ srv/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-
-""" from .models import Srv
-
-admin.site.register(Srv) """
